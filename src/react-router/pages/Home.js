39f0848ba508d13d6f7f7cc28008dd86fd711333
Onur Yurdagelen <yurdagelenonur7@gmail.com>
2022-05-12 22:05:06 +0300

    react-router implementation

---
+++ src/react-router/pages/Home.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <section className='section'>
+    <h2>Home</h2>
+  </section>
+  );
+};
+export default Home;
